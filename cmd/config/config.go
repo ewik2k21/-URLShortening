e5a3e057bfae5822d729288859fee269be8d1f08
ewik2k21 <ewik2k21@yandex.ru>
2023-10-18 18:04:17 +0300

    rename value

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -11,8 +11,8 @@ var FlagB string
 func ParseFlags() {
 	flag.StringVar(&FlagA, "a", ":8080", "port for server")
 	flag.StringVar(&FlagB, "b", "localhost"+FlagA, "base address result")
-	if baseUrl, err := os.LookupEnv("BASE_URL"); err {
-		FlagB = baseUrl
+	if baseURL, err := os.LookupEnv("BASE_URL"); err {
+		FlagB = baseURL
 	}
 	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
 		FlagA = envServerAddress
